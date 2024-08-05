6d111d3bb39a7fba22b21685f3d556570739b000
Anis Momin <anis_momin@hotmail.com>
2024-08-06 00:14:49 +0500

    first commit

---
+++ tests/test_first_function.py
@@ -0,0 +1,10 @@
+from project_python import main
+
+def test_my_first_function():
+    r = main.my_first_function()
+    assert r == "Hello World"
+
+
+def test_my_first_function_2():
+    r = main.my_first_function()
+    assert r == "Hello World"
